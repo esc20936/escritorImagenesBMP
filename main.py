30811470e643de2250affef52f70709ca8f37b82
esc20936 <esc20936@uvg.edu.gt>
2022-07-07 21:56:15 -0600

    sin, cos

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from cmath import cos
 from math import sin
 from gl import Render
 
-rend = Render(1024, 512)
+rend = Render(512, 512)
 
 rend.glClearColor(0,0.5,0)
 rend.glColor(1,1,0)
 rend.glClear()
-
+#
 for i in range(512):
     rend.glPoint(i, int(sin(i/10) * 100 + 100))
+rend.glColor(1,0,0)
 
+for i in range(512):
+    rend.glPoint(i, int(abs(cos(i/10) * 100 + 100)))
 # rend.glPoint(100,100)
 rend.glFinish("output.bmp")
